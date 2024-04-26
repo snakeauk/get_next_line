a87f314617baebbd60db2882ad12926d91e5a18c
稲村孝祐 <kinamura@MacBook-Pro.local>
2024-04-27 00:19:05 +0900

    get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -39,48 +39,48 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-#include <stdio.h>
-#include <fcntl.h>
-int	main(void)
-{
-	char	*line;
-	int		i = 1;
-	int		fd1;
-	int		fd2;
-	int		fd3;
+// #include <stdio.h>
+// #include <fcntl.h>
+// int	main(void)
+// {
+// 	char	*line;
+// 	int		i = 1;
+// 	int		fd1;
+// 	int		fd2;
+// 	int		fd3;
 
-	printf("-------------------\n");
-	fd1 = open("test/test.txt", O_RDONLY);
-	while (i < 7)
-	{
-		line = get_next_line(fd1);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	close(fd1);
-	fd2 = open("test/test2.txt", O_RDONLY);
-	printf("\n-------------------\n-------------------\n");
-	i = 1;
-	while (i < 7)
-	{
-		line = get_next_line(fd2);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	close(fd2);
-	fd3 = open("test/test3.txt", O_RDONLY);
-	printf("\n-------------------\n-------------------\n");
-	i = 1;
-	while (i < 7)
-	{
-		line = get_next_line(fd3);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	printf("\n-------------------\n");
-	close(fd3);
-	return (0);
-}
+// 	printf("-------------------\n");
+// 	fd1 = open("test/test.txt", O_RDONLY);
+// 	while (i < 7)
+// 	{
+// 		line = get_next_line(fd1);
+// 		printf("line [%02d]: %s", i, line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	close(fd1);
+// 	fd2 = open("test/test2.txt", O_RDONLY);
+// 	printf("\n-------------------\n-------------------\n");
+// 	i = 1;
+// 	while (i < 7)
+// 	{
+// 		line = get_next_line(fd2);
+// 		printf("line [%02d]: %s", i, line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	close(fd2);
+// 	fd3 = open("test/test3.txt", O_RDONLY);
+// 	printf("\n-------------------\n-------------------\n");
+// 	i = 1;
+// 	while (i < 7)
+// 	{
+// 		line = get_next_line(fd3);
+// 		printf("line [%02d]: %s", i, line);
+// 		free(line);
+// 		i++;
+// 	}
+// 	printf("\n-------------------\n");
+// 	close(fd3);
+// 	return (0);
+// }
