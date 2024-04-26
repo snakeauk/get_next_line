a2ec8e86f90b8d465ed2f25546986c79f91504ff
稲村孝祐 <kinamura@MacBook-Pro.local>
2024-04-27 00:02:36 +0900

    get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,19 @@
-
-
-#ifndef GET_NEXT_LINE.H
-# define GET_NEXT_LINE.H
-
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 #include <unistd.h>
 #include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
 char	*get_next_line(int fd);
+char	*ft_read_file(int fd, char *str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *str);
+char	*ft_new_str(char *str);
 
 #endif
